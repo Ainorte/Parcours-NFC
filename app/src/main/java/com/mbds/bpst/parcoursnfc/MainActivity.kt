250b73ce67a5e53c15d7457b7440e3bd522595f3
William POITEVIN <william.poitevin@gmail.com>
2021-03-08 22:36:32 +0100

    Auto Refreshing location google map

--- app/src/main/java/com/mbds/bpst/parcoursnfc/MainActivity.kt
+++ app/src/main/java/com/mbds/bpst/parcoursnfc/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.mbds.bpst.parcoursnfc
 
+import android.Manifest.permission
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
-import androidx.annotation.DrawableRes
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.forEach
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.mbds.bpst.parcoursnfc.databinding.ActivityMainBinding
 import com.mbds.bpst.parcoursnfc.fragments.PlayFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +24,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(baseContext))
-        changeFragment(PlayFragment(), false)
+        if( ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            //Demander la permission d'utiliser le GPS
+            if(shouldShowRequestPermissionRationale(permission.ACCESS_FINE_LOCATION)){
+                //Nous avons déjà demandé la permission
+                explainForPermission()
+            }else{
+                //On demande pour la première fois
+                askForPermission()
+            }
+        }else{
+            //c'est ok pour le GPS
+            changeFragment(PlayFragment(), false)
+        }
     }
 
     fun changeFragment(fragment: Fragment, addToBackStack: Boolean = true) {
@@ -29,4 +46,55 @@ class MainActivity : AppCompatActivity() {
                 addToBackStack(null)
         }.commit()
     }
+
+    private fun askForPermission() {
+        requestPermissions(arrayOf(permission.ACCESS_FINE_LOCATION), 0)
+    }
+
+    private fun explainForPermission() {
+        Snackbar.make(
+            findViewById(android.R.id.content),
+            "Autoriser le GPS pour utiliser l'application",
+            Snackbar.LENGTH_LONG
+        ).setAction("Autoriser")
+            { askForPermission() }
+        .show()
+    }
+
+    private fun explainForOption(){
+        Snackbar.make(findViewById(android.R.id.content), "Vous devez autoriser le GPS dans les paramètres", Snackbar.LENGTH_LONG)
+            .setAction("Paramètres") {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri: Uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }.show()
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ){
+        if(requestCode == 0)
+        {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                //Ok pour le GPS
+                changeFragment(PlayFragment(), false)
+            }
+            else if (shouldShowRequestPermissionRationale(permission.ACCESS_FINE_LOCATION)) {
+                // permission refusée avec demande de ne pas redemander
+                explainForOption()
+            }
+            else
+            {
+                // permission refusée
+                explainForPermission()
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/mbds/bpst/parcoursnfc/fragments/PlayFragment.kt
+++ app/src/main/java/com/mbds/bpst/parcoursnfc/fragments/PlayFragment.kt
@@ -1,22 +1,91 @@
 package com.mbds.bpst.parcoursnfc.fragments
 
+import android.annotation.SuppressLint
+import android.icu.text.Transliterator
+import android.location.Location
+import android.location.LocationListener
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.mbds.bpst.parcoursnfc.R
 import com.mbds.bpst.parcoursnfc.databinding.FragmentPlayBinding
 
-class PlayFragment : Fragment() {
+
+class PlayFragment : Fragment(){
 
     private lateinit var binding: FragmentPlayBinding
+    private lateinit var googleMap: GoogleMap
+    private lateinit var location: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        fusedLocationClient = context?.let { LocationServices.getFusedLocationProviderClient(it) }!!
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                location = locationResult.lastLocation
+
+                var position = LatLng(location.latitude, location.longitude)
+
+                googleMap.clear()
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(position))
+                googleMap.addMarker(MarkerOptions().position(position))
+            }
+        }
+
+        locationRequest = LocationRequest.create()?.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }!!
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentPlayBinding.inflate(inflater, container, false)
+        var mapFragment =  childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(callback)
         return binding.root
     }
+
+    @SuppressLint("MissingPermission")
+    override fun onResume() {
+        super.onResume()
+         fusedLocationClient.requestLocationUpdates(
+             locationRequest,
+             locationCallback,
+             Looper.getMainLooper())
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    @SuppressLint("MissingPermission")
+    private val callback = OnMapReadyCallback { googleMap ->
+        this.googleMap = googleMap
+        this.googleMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f))
+    }
+
 }
