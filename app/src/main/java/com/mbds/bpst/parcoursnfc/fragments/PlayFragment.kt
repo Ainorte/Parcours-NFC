600793202880e4dc07f1be2b87c262acfe53b9fd
Mac William <clement.merlet26@gmail.com>
2021-03-09 11:28:16 +0100

    fix CreateFragment

--- app/src/main/java/com/mbds/bpst/parcoursnfc/fragments/PlayFragment.kt
+++ app/src/main/java/com/mbds/bpst/parcoursnfc/fragments/PlayFragment.kt
@@ -51,7 +51,7 @@ class PlayFragment : Fragment(){
         }
 
         locationRequest = LocationRequest.create()?.apply {
-            interval = 10000
+            interval = 5000
             fastestInterval = 5000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }!!
