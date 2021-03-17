8cb453e26bc3c8ba684231e1e199a3c5d35be37f
William POITEVIN <william.poitevin@gmail.com>
2021-03-17 22:07:07 +0100

    add read

--- app/src/main/java/com/mbds/bpst/parcoursnfc/fragments/PlayFragment.kt
+++ app/src/main/java/com/mbds/bpst/parcoursnfc/fragments/PlayFragment.kt
@@ -2,7 +2,9 @@ package com.mbds.bpst.parcoursnfc.fragments
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.location.Location
+import android.net.Uri
 import android.nfc.NdefMessage
 import android.nfc.Tag
 import android.nfc.tech.Ndef
@@ -14,18 +16,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.mbds.bpst.parcoursnfc.MainActivity
 import com.mbds.bpst.parcoursnfc.R
+import com.mbds.bpst.parcoursnfc.data.entities.Etape
 import com.mbds.bpst.parcoursnfc.data.models.EtapeViewModel
 import com.mbds.bpst.parcoursnfc.databinding.FragmentPlayBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.UnsupportedEncodingException
-import kotlin.experimental.and
 
 
 class PlayFragment : Fragment(), ActionNFC{
@@ -36,8 +44,11 @@ class PlayFragment : Fragment(), ActionNFC{
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
     private lateinit var locationRequest: LocationRequest
+    private lateinit var etapeViewModel: EtapeViewModel
     private var firstLoc = true
     private lateinit var viewModel: EtapeViewModel
+    private var lastEtape: Etape? = null
+    private var lastMarker:MarkerOptions? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,11 +72,13 @@ class PlayFragment : Fragment(), ActionNFC{
             fastestInterval = 5000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }!!
+
+        etapeViewModel = (activity as MainActivity).etapeViewModel
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         viewModel = (activity as MainActivity).etapeViewModel
         binding = FragmentPlayBinding.inflate(inflater, container, false)
@@ -73,7 +86,11 @@ class PlayFragment : Fragment(), ActionNFC{
         mapFragment.getMapAsync(callback)
 
         binding.navigateButton.setOnClickListener {
-            //TODO : Intent navigation
+            if(lastEtape != null){
+                val gmmIntentUri: Uri = Uri.parse("geo:${location.latitude},${location.longitude}?q=" + Uri.encode("${lastEtape!!.location.latitude},${lastEtape!!.location.longitude}"))
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                startActivity(mapIntent)
+            }
         }
 
         return binding.root
@@ -83,15 +100,28 @@ class PlayFragment : Fragment(), ActionNFC{
     override fun onResume() {
         super.onResume()
          fusedLocationClient.requestLocationUpdates(
-             locationRequest,
-             locationCallback,
-             Looper.getMainLooper())
+                 locationRequest,
+                 locationCallback,
+                 Looper.getMainLooper())
         activity?.title = "Parcours NFC"
 
         var act = activity as MainActivity
-        act.setMenuButtonVisibility(R.id.createItem,true)
-        act.setMenuButtonVisibility(R.id.resetItem,true)
-        act.onReset = null
+        act.setMenuButtonVisibility(R.id.createItem, true)
+        act.setMenuButtonVisibility(R.id.resetItem, true)
+        act.onReset = {
+            lifecycleScope.launch {
+                withContext(Dispatchers.IO)
+                {
+                    val etapes = etapeViewModel.getAllEtapeByRead(true)
+                    etapes.forEach { etape -> etapeViewModel.deleteEtape(etape) }
+                    withContext(Dispatchers.Main){
+                        lastEtape = null
+                        googleMap.clear();
+                        Toast.makeText(context, "Parcours remis à zero", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
 
     override fun onPause() {
@@ -104,6 +134,23 @@ class PlayFragment : Fragment(), ActionNFC{
         this.googleMap = googleMap
         this.googleMap.moveCamera(CameraUpdateFactory.zoomTo(17.0f))
         this.googleMap.isMyLocationEnabled = true
+
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO)
+            {
+                val etapes = etapeViewModel.getAllEtapeByRead(true)
+                lastEtape = etapes.lastOrNull()
+                withContext(Dispatchers.Main){
+                    if(etapes.isNotEmpty()){
+                        etapes.subList(0, etapes.lastIndex - 1).forEach { etape -> googleMap.addMarker(MarkerOptions().position(etape.location).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))) }
+                        lastMarker = lastEtape?.location?.let { MarkerOptions().position(it).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)) }
+                        googleMap.addMarker(lastMarker)
+
+                        binding.description.text = lastEtape!!.indice
+                    }
+                }
+            }
+        }
     }
 
     override fun onNFC(tag: Tag, ndef: Ndef, rawMsgs: Array<Parcelable>?, context: Context) {
@@ -117,16 +164,47 @@ class PlayFragment : Fragment(), ActionNFC{
                 val payload = ndefRecord.payload
                 try {
                     val type = ndefRecord.toMimeType()
-                    val recordTxt = String(
-                        payload, 0,
-                        payload.size,
-                        charset("UTF-8"))
-                    all += recordTxt
+                    if(type == "application/parcoursnfc"){
+                        val recordTxt = String(
+                                payload, 0,
+                                payload.size,
+                                charset("UTF-8"))
+                        all += "$recordTxt||"
+                    }
                 } catch (e: UnsupportedEncodingException) {
                     e.printStackTrace()
                 }
 
-                Toast.makeText(context, all, Toast.LENGTH_SHORT).show()
+                if(lastEtape == null ){
+                    val etape = Etape("Depart", LatLng(location.latitude, location.longitude), true)
+                    etapeViewModel.insert(etape)
+                    googleMap.addMarker(MarkerOptions().position(etape.location).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
+                }
+
+                val splitedAll = all.split("||")
+
+                var loc = when (splitedAll[0] == "")
+                {
+                    true -> {
+                        lastEtape?.location
+                    }
+                    false -> {
+                        val split = splitedAll[0].split(";")
+                        LatLng(split[0].toDouble(), split[1].toDouble())
+                    }
+                }
+
+                loc?.let {
+                    lastEtape =  Etape(splitedAll[1], it, true)
+                    etapeViewModel.insert(lastEtape!!)
+                    lastMarker?.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                    lastMarker = MarkerOptions().position(lastEtape!!.location).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                    googleMap.addMarker(lastMarker)
+
+                    binding.description.text = lastEtape!!.indice
+                }
+
+                //Toast.makeText(context, all, Toast.LENGTH_SHORT).show()
             }
         }
     }
